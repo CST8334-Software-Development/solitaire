96c7a9698445663c24886fe8f41d9759a425e0cf
Pascal-Ladouceur <84280647+Pascal-Ladouceur@users.noreply.github.com>
2023-03-05 14:51:44 -0500

    Added 3 cards to WastePile

--- solitaire/solitaire/WastePile.java
+++ solitaire/solitaire/WastePile.java
@@ -1,11 +1,14 @@
 package solitaire;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.beans.PropertyChangeSupport;
 
 public class WastePile extends CardPile{
 	private PropertyChangeSupport propertyChangeSupport;
+	public static final int CASCADE_GAP=20;
 
 	public WastePile() {
 		super();
@@ -16,4 +19,34 @@ public class WastePile extends CardPile{
 			}
 		});
 	}
+	
+	//***********************************Put 3 cards instead of 1 mod Feb 25********************************************
+	
+	public boolean canPutOnTop(Card aCard) {
+		return true;
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		int cardPosition=0;
+		if (this.getActualSize() > 0) {
+			if (this.getActualSize()<4) {
+				for (int i = 0;i<this.getActualSize();i++) {
+					Card aCard = cards.get(i);
+					aCard.setPosition(i*CASCADE_GAP, 0);
+					aCard.paintComponent(g);
+				}
+			} else {
+				for (int i = this.getActualSize()-3;i<this.getActualSize();i++) {
+					Card aCard = cards.get(i);
+					aCard.setPosition(cardPosition, 0);
+					aCard.paintComponent(g);
+					cardPosition = cardPosition + 20;
+				}
+			}
+		}
+		cardPosition = 0;	
+	}
+	
 }
